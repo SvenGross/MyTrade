3a72752017dd13ae25fc0a65a4c6162662c104e7
sacha.weiersmueller <sacha.weiersmueller@Santis-SAN26.sanschoolsbsdom.local>
2015-07-01 15:24:36 +0200

    Meldungsimplementation :beer:

--- src/error/Meldungen.java
+++ src/error/Meldungen.java
@@ -3,19 +3,21 @@ package error;
 import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 import model.KonstantenSession;
 
 @ManagedBean
+@SessionScoped
 public class Meldungen {
 
 	private static ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-	public  static Map<String, Object> sessionMap  = externalContext.getSessionMap();
+	public  Map<String, Object> sessionMap  = externalContext.getSessionMap();
 
 	public static String AKTIE_ERSTELLEN        = "Die Aktie wurde erfolgreich gespeichert: ";
-	public static String DIVIDENDE_AUSSCHUETTEN = "Die Dividende wurde erfolgreich ausgeschüttet";
+	public static String DIVIDENDE_AUSSCHUETTEN = "Die Dividende wurde erfolgreich ausgeschüttet.";
 	public static String BENUTZER_ANLEGEN       = "Der Benutzer wurde erfolgreich angelegt: ";
 	public static String AUFTRAG_AUSFUEHREN     = "Der Auftrag wurde erfolgreich ausgeführt: ";
 	public static String AUFTRAG_ERFASSEN       = "Der Auftrag wurde erfolgreich erfasst: ";
