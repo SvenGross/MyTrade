52c7eaedabb6baedb503faf39ec00ce51583adc9
grosssven <Sven.Gross@Santis-SAN22.sanschoolsbsdom.local>
2015-07-01 13:09:23 +0200

    aktie model

--- src/model/Aktie.java
+++ src/model/Aktie.java
@@ -2,13 +2,15 @@ package model;
 
 public class Aktie {
 
+	private int aktienID;
 	private String name;
 	private String symbol;
 	private double preis;
 	private double dividende;
 	private int stueck;
 		
-	public Aktie(String name, String symbol, double preis, double dividende, int stueck) {
+	public Aktie(int aktienID, String name, String symbol, double preis, double dividende, int stueck) {
+		this.aktienID = aktienID;
 		this.name = name;
 		this.symbol = symbol;
 		this.preis = preis;
@@ -16,41 +18,50 @@ public class Aktie {
 		this.stueck = stueck;
 	}
 	
-	public String back() {
-		return "aktieErfassen?faces-redirect=true";
+	public int getAktienID() {
+		return aktienID;
 	}
 	
-	public void save() {
-		//TODO
+	public void setAktienID(int aktienID) {
+		this.aktienID = aktienID;
 	}
 	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getSymbol() {
 		return symbol;
 	}
+	
 	public void setSymbol(String symbol) {
 		this.symbol = symbol;
 	}
+	
 	public double getPreis() {
 		return preis;
 	}
+	
 	public void setPreis(double preis) {
 		this.preis = preis;
 	}
+	
 	public double getDividende() {
 		return dividende;
 	}
+	
 	public void setDividende(double dividende) {
 		this.dividende = dividende;
 	}
+	
 	public int getStueck() {
 		return stueck;
 	}
+	
 	public void setStueck(int stueck) {
 		this.stueck = stueck;
 	}
